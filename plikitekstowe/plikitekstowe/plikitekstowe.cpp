9e8de6c1869e316aee551e9d24e79f3618c0cf96
JanGrzegorski <jan.grzegorski3@gmail.com>
2023-10-31 10:24:22 +0100

    zadanka

---
+++ plikitekstowe/plikitekstowe/plikitekstowe.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    fstream plik;
+    string linia;
+    plik.open("dane.txt", ios::in);
+    int sumap = 0;
+    int sumanp = 0;
+
+    if (plik.good() == true)
+    {
+        cout << "MOM FILE CHLOPIE" << endl;
+        while (getline(plik, linia))
+        {
+            if (stoi(linia) % 2 == 0)
+            {
+               sumap = sumap + stoi(linia);
+            }
+            else
+            {
+                sumanp = sumanp + stoi(linia);
+            }
+        }
+        cout << "Suma liczb parzystych = " << sumap << endl;
+        cout << "Suma liczb nie pazrtstych = " << sumanp << endl;
+    }
+    else
+    {
+        cout << "NI MOM FILE CHLOPIE" << endl;
+    }
+
+    plik.close();
+
+    return 0;
+}
---
+++ tabliczkamnozenia/tabliczkamnozenia/tabliczkamnozenia.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	/*for (int i = 1; i < 11; i++)
+	{
+		if (i == 1) cout.width(5);
+		for (int j = 1; j < 11; j++)
+		{
+			
+		}
+		cout.width(5);  cout << endl;
+	}*/
+	int i = 1;
+
+	/*while (i <= 10)
+	{
+		if (i == 1) cout.width(5);
+		int j = 1;
+		while (j <= 10)
+		{ 
+		cout.width(5);  cout << i * j;
+		j++;	
+		}
+		cout.width(5);  cout << endl;
+		i++;
+	}*/
+	do
+	{
+		if (i == 1) cout.width(5);
+		int j = 1;
+		while (j <= 10)
+		{
+			cout.width(5);  cout << i * j;
+			j++;
+		}
+		cout.width(5);  cout << endl;
+		i++;
+	} while (i <= 10);
+
+    return 0;
+}
